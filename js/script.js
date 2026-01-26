919e69e4fcf277d2794ae7dd4acd23d3c9556f08
Kuempel <kuempema@oregonstate.edu>
2026-01-26 15:47:02 -0800

    mapping stuff added

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ fetch('/data/attributed_ports.geojson')
 .then(data => {
     console.log(data);
 })
+
+/* Example from Leaflet Quick Start Guide*/
+
+var map = L.map('map').setView([0, 0], 4);
+
+//add tile layer
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   maxZoom: 19,
+   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+map.on('click', onMapClick);
