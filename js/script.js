6baf9134644d356eac48f6375994814900c0cbf4
Kuempel <kuempema@oregonstate.edu>
2026-01-28 10:52:03 -0800

    it brokey :(

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-fetch('/data/attributed_ports.geojson')
+function FetchPort(){ fetch('/data/attributed_ports.geojson')
 .then(response => response.json())
-.then(data => {
-    console.log(data);
-})
-
+.then(return data )  
+}
 /* Example from Leaflet Quick Start Guide*/
 
 var map = L.map('map').setView([0, 0], 4);
@@ -15,4 +13,19 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-map.on('click', onMapClick);
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [0,0]
+    }
+};
+
+L.geoJSON(geojsonFeature).addTo(map);
+
+L.geoJSON(FetchPort()).addTo(map);
