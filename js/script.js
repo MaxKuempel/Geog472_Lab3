3a81e2fe3e978ec5dee6cbac610e6018fdfd8aee
Kuempel <kuempema@oregonstate.edu>
2026-02-02 15:32:36 -0800

    Proportional symbols for ports working!

--- js/script.js
+++ js/script.js
@@ -1,23 +1,50 @@
-function FetchPort(){ fetch('/data/attributed_ports.geojson')
+let ports;
+
+fetch('/data/attributed_ports.geojson')
 .then(response => response.json())
-.finally(response => {
-    L.geoJSON(response).addTo(map);
+.then(data => {
+    ports = data;
     console.log("Ports Fetched!");
+    console.log(data);
+    
 })
-}
+.catch(error => console.error('Error:', error));
 
 
 
 /* Example from Leaflet Quick Start Guide*/
 
-var map = L.map('map').setView([0, 0], 4);
+var PortMap = L.map('PortMap').setView([0, 0], 4);
 
 //add tile layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
    maxZoom: 19,
    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(PortMap);
 
 
-//L.geoJSON(geojsonFeature).addTo(map);
-FetchPort()
+
+
+function xyflip(coordset){
+    return [coordset[1],coordset[0]]
+}
+
+
+function DisplayPorts(){
+for (let i = 0; i < ports.features.length; i++){
+
+    var circle = L.circle(xyflip(ports.features[i].geometry.coordinates), 
+        {
+        color: 'black',
+        fillColor: 'rgba(0, 214, 252, 1)',
+        fillOpacity: 0.7,    radius: (ports.features[i].properties.outflows) / 500
+        }).addTo(PortMap).bindPopup(
+            ports.features[i].properties.Name + ", " + ports.features[i].properties.Country
+        );
+
+}
+}
+
+window.onload = function(){
+DisplayPorts();
+}
