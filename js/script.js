3f8341c6f8a024110facebdc3ebd34367c422fbd
Kuempel <kuempema@oregonstate.edu>
2026-02-06 10:52:41 -0800

    still kinda broken

--- js/script.js
+++ js/script.js
@@ -11,16 +11,20 @@ fetch('data/attributed_ports.geojson')
 })
 .catch(error => console.error('Error:', error));
 }
-
-
+window.addEventListener('DOMContentLoaded', function() {
+    FetchPorts()
+    console.log("fetch ports debug")
+});
 /* Example from Leaflet Quick Start Guide*/
 
 var PortMap = L.map('PortMap').setView([0, 0], 4);
 
+
+
 //add tile layer
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-   maxZoom: 19,
-   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+var OpenStreetMap_HOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
 }).addTo(PortMap);
 
 
@@ -30,15 +34,15 @@ function xyflip(coordset){
     return [coordset[1],coordset[0]]
 }
 
-
-function DisplayPorts(){
+ports
+function DisplayPorts(ports){
 for (let i = 0; i < ports.features.length; i++){
 
     var circle = L.circle(xyflip(ports.features[i].geometry.coordinates), 
         {
         color: 'black',
         fillColor: 'rgba(0, 214, 252, 1)',
-        fillOpacity: 0.7,    radius: (ports.features[i].properties.outflows) / (400 * PortMap.getZoom())
+        fillOpacity: 0.7,    radius: (ports.features[i].properties.outflows) / (400)
         }).addTo(PortMap).bindPopup(
             ports.features[i].properties.Name + ", " + ports.features[i].properties.Country
         );
@@ -47,7 +51,9 @@ for (let i = 0; i < ports.features.length; i++){
 }
 
 window.onload = function(){
-FetchPorts();
-DisplayPorts();
-console.log(PortMap.getZoom());
+    console.log("working");
+    FetchPorts();
+    console.log(ports);
+    DisplayPorts(ports);
+    console.log("last");
 }
